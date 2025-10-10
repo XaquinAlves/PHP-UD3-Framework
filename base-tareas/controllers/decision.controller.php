d046fdc106116a21f32ba2144e15d2b896796f15
XaquinAlves <xaquinalves@gmail.com>
2025-10-10 12:29:54 +0200

    Ejercicios Decision PHP

--- base-tareas/controllers/decision.controller.php
+++ base-tareas/controllers/decision.controller.php
@@ -5,12 +5,34 @@
 // Xeral
 $data['titulo'] = "Exercicios de estructuras de decision";
 
-//Exercicio 1
+// Exercicio 1
 $data["div_titulo_ex1"] = "Exercicio 1";
 $data['num1'] = 28;
 $data['num2'] = 14;
 $data["e_divisible"] = ($data['num1'] % $data['num2'] == 0);
 var_dump($data["e_divisible"]);
+
+// Exercicio 2
+$data["div_titulo_ex2"] = "Exercicio 2";
+$data['n1'] = 2;
+$data['n2'] = 5;
+$data['n3'] = 3;
+$data['maximo'] = max($data['n1'], $data['n2'], $data['n3']);
+
+// Exercicio 3
+$data["div_titulo_ex3"] = "Exercicio 3";
+$data['segundos_iniciales'] = 264312;
+$data['dias'] = intval($data['segundos_iniciales'] / 86400);
+$data['horas'] = intval(($data['segundos_iniciales'] % 8640) / 3600);
+$data['minutos'] = intval(($data['segundos_iniciales']  % 3600) / 60);
+$data['segundos'] = $data['segundos_iniciales'] % 60;
+
+// Exercicio 4
+$data["div_titulo_ex4"] = "Exercicio 4";
+$data['ano'] = 2025;
+$data['es_bisiexto'] = ($data['ano'] % 4 == 0 && $data['ano'] % 100 != 0) || $data['ano'] % 400 == 0;
+
+
 
 /*
  * Llamamos a las vistas
--- base-tareas/views/decision.view.php
+++ base-tareas/views/decision.view.php
@@ -7,7 +7,7 @@
 <!-- Content Row -->
 
 <div class="row">
-
+    <!-- Exercicio 1 -->
     <div class="col-6">
         <div class="card shadow mb-4">
             <div
@@ -17,6 +17,58 @@
             <div class="card-body">
                 <?= $data['num1']?> <?= $data['e_divisible'] ? "" : "NON "?> é divisible entre
                 <?= $data['num2']?>
+            </div>
+        </div>
+    </div>
+    <!-- Exercicio 2 -->
+    <div class="col-6">
+        <div class="card shadow mb-4">
+            <div
+                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary"><?php echo $data['div_titulo_ex2']; ?></h6>
+            </div>
+            <div class="card-body">
+                <?= $data['n1'] == $data['maximo'] ? "<strong>$data[n1]</strong>" :  $data['n1'] ?>
+                <?= $data['n2'] == $data['maximo'] ? "<strong>" . $data['n2'] . "</strong>" :  $data['n2'] ?>
+                <?= $data['n3'] == $data['maximo'] ? "<strong>" . $data['n3'] . "</strong>" :  $data['n3'] ?>
+            </div>
+        </div>
+    </div>
+    <!-- Exercicio 3 -->
+    <div class="col-6">
+        <div class="card shadow mb-4">
+            <div
+                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary"><?php echo $data['div_titulo_ex3']; ?></h6>
+            </div>
+            <div class="card-body">
+                <h6>Segundos como entrada: <?= $data['segundos_iniciales'] ?></h6>
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Días</th>
+                        <th>Horas</th>
+                        <th>Minutos</th>
+                        <th>Segundos</th>
+                    </tr>
+                    <tr>
+                        <td><?=$data['dias']?></td>
+                        <td><?=$data['horas']?></td>
+                        <td><?=$data['minutos']?></td>
+                        <td><?=$data['segundos']?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- Exercicio  4 -->
+        <div class="col-6">
+        <div class="card shadow mb-4">
+            <div
+                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary"><?php echo $data['div_titulo_ex4']; ?></h6>
+            </div>
+            <div class="card-body">
+                O ano <?= $data['ano'] ?> <?= $data['es_bisiesto'] ? "" : "NON" ?> é bisiexto
             </div>
         </div>
     </div>
